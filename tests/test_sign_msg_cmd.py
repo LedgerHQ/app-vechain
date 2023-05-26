82fe24a91e9e22988652d71052b652cc252db14e
tony <liboliqi@gmail.com>
2023-05-26 12:12:31 +0800

    [tests]: add some random generated test cases

--- tests/test_sign_msg_cmd.py
+++ tests/test_sign_msg_cmd.py
@@ -45,24 +45,25 @@ def test_sign_message(firmware, backend, navigator, test_name):
             # check that the message hash computed on device is the same as the
             # calculated one (check only the first displayed digits)
             navigator.navigate_until_text_and_compare(NavInsID.RIGHT_CLICK,
-                                                      [NavInsID.BOTH_CLICK],
-                                                      str(hash_part_to_check).upper(),
-                                                      ROOT_SCREENSHOT_PATH,
-                                                      test_name,
-                                                      screen_change_after_last_instruction=False)
+                                                        [NavInsID.BOTH_CLICK],
+                                                        str(hash_part_to_check).upper(),
+                                                        ROOT_SCREENSHOT_PATH,
+                                                        test_name,
+                                                        screen_change_after_last_instruction=False)
             navigator.navigate_until_text(NavInsID.RIGHT_CLICK,
                                             [NavInsID.BOTH_CLICK],
                                             "Sign",
                                             screen_change_before_first_instruction=False)
-        else:
+        else:            
+            # check that the message hash computed on device is the same as the
+            # reference one (check only the first displayed digits)
             navigator.navigate_until_text_and_compare(NavInsID.USE_CASE_REVIEW_TAP,
-                                                      [NavInsID.USE_CASE_REVIEW_CONFIRM,
-                                                       NavInsID.USE_CASE_STATUS_DISMISS, 
-                                                       NavInsID.WAIT_FOR_HOME_SCREEN],
-                                                      "Hold to sign",
-                                                      ROOT_SCREENSHOT_PATH,
-                                                      test_name)
-
+                                                        [NavInsID.USE_CASE_REVIEW_TAP,
+                                                        NavInsID.USE_CASE_REVIEW_CONFIRM,
+                                                        NavInsID.USE_CASE_STATUS_DISMISS],
+                                                        str(hash_part_to_check).upper(),
+                                                        ROOT_SCREENSHOT_PATH,
+                                                        test_name)
     # The device as yielded the result, parse it and ensure that the signature is correct
     response = client.get_async_response().data
 
@@ -146,3 +147,102 @@ def test_sign_message_cancel(firmware, backend, navigator, test_name):
             # Assert that we have received a refusal
             assert response.status == Errors.SW_TRANSACTION_CANCELLED
             assert len(response.data) == 0
+
+# In this test we generated some random message for the device to sign and validate it on screen.
+def test_sign_random_message(firmware, backend, navigator, test_name):
+    messages = [
+        "psst ouch although oof industry until phew",
+        "personify trifling lest brr judgementally phew daintily healthily",
+        "how clear-cut wherever lest",
+        "smart below dishwasher during",
+        "ew elaborate invalidate aha impediment hiccough",
+        "hm giving when ton prospect",
+        "waterski abate truthfully lest neatly which careful",
+        "unless wave vitality orderly whose awesome sharply",
+        "quadruple unnecessarily shelter pro yippee",
+        "yuck needily minus dismal",
+        "meh toward wonderfully leading squiggly",
+        "psst inasmuch prune baseboard guilder",
+        "for instead leading upon closely oh fooey correctly",
+        "geez direct pace slowly rapidly whimsical",
+        "approval phooey yield than yuck once vacantly so",
+        "meanwhile during zowie miracle",
+        "abaft utterly despite tailor unless apprehension poll",
+        "cruelly alongside youthful furthermore ouch longingly troubleshoot",
+        "when aw very fondly wrapping until ugh",
+        "drafty irritating supposing violet athwart ugh landing until",
+        "whereas graduate skateboard adjust outside whether anyone",
+        "unwilling beside above wherever now aha duh",
+        "standardize keep psst yearningly",
+        "sweatshirt ancestor lily aw toward eek pfft traumatic",
+        "colorless smoking after against pointed anti",
+        "around phew contract major alfalfa",
+        "educated roasted zowie since",
+        "lest judgementally yearly scoop woot zowie supposing",
+        "fancy ouch patio whoa curtain prize",
+        "ample burn-out denote scream",
+        "terrific phooey anti weepy marginalize incline from although",
+        "icecream early that hence defiant aside amid",
+        "hastily who while absent",
+        "defog loosely livid essential usually tattered",
+        "well whenever ick sophisticated yowza handgun inveigh down",
+        "but onto daily gosh recompense twine bah crumble",
+        "duh grizzled instead vice incidentally attitude sociable",
+        "yowza fig range whoever",
+        "phooey but if because ack patiently painfully athwart",
+        "famously boo throughout powerfully inside or zowie but"
+    ]
+
+    # Use the app interface instead of raw interface
+    client = VechainClient(backend)
+    # Disable raising when trying to unpack an error APDU
+    backend.raise_policy = RaisePolicy.RAISE_NOTHING
+
+    # get public key from device
+    response = client.get_public_key(path=path).data
+    _, public_key = unpack_get_public_key_response(response)
+
+    for i, msg in enumerate(messages):
+        # as stax tests takes more time, run the first 5 tests only
+        if i>4 and firmware.device.startswith("stax"):
+            break
+
+        message_encoded = msg.encode()
+        # prepare the message to send
+        message_bytes = struct.pack(">I", len(message_encoded))
+        message_bytes += message_encoded
+
+        hash_msg = blake2b(digest_size=32)
+        hash_msg.update(toPersonalMessage(msg))
+        hash_msg = hash_msg.digest().hex()
+        hash_part_to_check = hash_msg[:4]
+
+        # Send the sign device instruction.
+        # As it requires on-screen validation, the function is asynchronous.
+        # It will yield the result when the navigation is done
+        with client.sign_message(path=path, data=message_bytes):
+            # Validate the on-screen request by performing the navigation appropriate for this device
+            if firmware.device.startswith("nano"):
+                # check that the message hash computed on device is the same as the
+                # calculated one (check only the first displayed digits)
+                navigator.navigate_until_text(NavInsID.RIGHT_CLICK,
+                                                [NavInsID.BOTH_CLICK],
+                                                str(hash_part_to_check).upper(),
+                                                screen_change_after_last_instruction=False)
+                navigator.navigate_until_text(NavInsID.RIGHT_CLICK,
+                                                [NavInsID.BOTH_CLICK],
+                                                "Sign",
+                                                screen_change_before_first_instruction=False)
+            else:
+                navigator.navigate_until_text(NavInsID.USE_CASE_REVIEW_TAP,
+                                                    [NavInsID.USE_CASE_REVIEW_CONFIRM,
+                                                    NavInsID.USE_CASE_STATUS_DISMISS, 
+                                                    NavInsID.WAIT_FOR_HOME_SCREEN],
+                                                    "Hold to sign")
+
+        # The device as yielded the result, parse it and ensure that the signature is correct
+        response = client.get_async_response().data
+
+        if isinstance(backend, SpeculosBackend):
+            assert check_signature_validity(public_key, response, toPersonalMessage(msg))
+    
