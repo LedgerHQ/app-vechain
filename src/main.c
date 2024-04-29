eb23edb13acc95e5a8fadcdad0e9902671313c81
LucaGenoni <lucageno9619@gmail.com>
2024-04-29 16:21:20 +0200

    set default contract data on

--- src/main.c
+++ src/main.c
@@ -1426,7 +1426,7 @@ __attribute__((section(".boot"))) int main(void) {
 
                 if (N_storage.initialized != 0x01) {
                     internalStorage_t storage;
-                    storage.dataAllowed = 0x00;
+                    storage.dataAllowed = 0x01;
                     storage.multiClauseAllowed = 0x00;
                     storage.initialized = 0x01;
                     nvm_write((void *)&N_storage, &storage, sizeof(internalStorage_t));
